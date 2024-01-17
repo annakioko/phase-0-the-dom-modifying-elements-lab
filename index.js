779a88c32bd6e2883f68d09dc415f721f6259359
Anna Kioko <annakioko00@gmail.com>
2024-01-17 06:47:41 +0300

    completed message

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const testVar = "Hi"
+
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory"
+
+newHeader.innerHTML = "Anna is the champion!";
